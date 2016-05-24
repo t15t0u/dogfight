c3c24c2884a635397c93fc2e96e6da9daefe0bf6
letis <letis@PC-B-PRIEUR>
2016-05-24 10:46:48 +0200

    Phase 3

---
+++ src/jpu2016/dogfight/controller/IOrderPerform.java
@@ -0,0 +1,6 @@
+package jpu2016.dogfight.controller;
+
+public interface IOrderPerform {
+	public void orderPerform();
+	
+}
---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,20 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder {
+	private int player;
+	
+	public void UserOrder(int player, Order order) {
+		
+	}
+	
+	public Order getOrder() {
+		Order UP = Order.UP;
+		
+		
+		return UP;
+	}
+	
+	public int getPlayer() {
+		return 1;
+	}
+}
