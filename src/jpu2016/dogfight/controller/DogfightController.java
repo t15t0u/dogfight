d03c4929d491ffc7fad3ca520deced4f32806560
letis <letis@PC-B-PRIEUR>
2016-05-24 10:20:04 +0200

    Phase 2

---
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -0,0 +1,31 @@
+package jpu2016.dogfight.controller;
+
+public class DogfightController {
+	
+	private final int TIME_SLEEP = 30;
+	
+	public DogfightController() {
+		
+	}
+		
+	public void orderPerform() {
+		
+	}
+	
+	public void play() {
+		
+	}
+	
+	public void setViewSystem() {
+		
+	}
+	
+	private void launchMissile() {
+		
+	}
+	
+	private void gameloop() {
+		
+	}
+	
+}
